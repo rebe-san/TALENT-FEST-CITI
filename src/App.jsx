bad7810eca72342a4cd01a80854cc51f4babb550
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-12-01 16:44:18 -0600

    Agregando componente de firma. Falta agregar botón de borrar para limpiar el canva

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Signature from './components/Signature'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Aqui va nuestra app</h1>
+      <Signature/>
     </div>
   );
 }
---
+++ src/components/Signature/index.jsx
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import SignatureCanvas from 'react-signature-canvas';
+
+import './style.css'
+
+const Signature = () => (
+    <Fragment>
+        <div className="signature-box">
+            <SignatureCanvas penColor='black'
+            canvasProps={{width: 300, height: 300, className: 'sigCanvas'}} />
+        </div>
+        <p>Firma</p>
+    </Fragment>
+)
+
+export default Signature;
